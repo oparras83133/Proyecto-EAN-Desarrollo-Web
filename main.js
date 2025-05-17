09b9ea2a907df14017afdde16dc5599248e87177
oparras83133 <oparras83133@universidadean.edu.co>
2025-05-17 15:27:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -239,5 +239,13 @@ document.querySelector('.btn-button1').addEventListener('click', function () {
   document.getElementById('inicia-sesion').style.display = 'block';
 });
 
-
+// Validación de inicio de sesion
+ function login() 
+  var usuario = document.getElementById('usuario').value;
+  var contraseña = document.getElementById('contraseña').value;
+  if (usuario == "EAN" && contraseña == "EAN") {
+        // Mostrar mensaje de inicio sesion exitosa
+          mensajeSesion.style.display = 'block';
+  } else {
+    alert("Usuario o contraseña incorrectos");
 
